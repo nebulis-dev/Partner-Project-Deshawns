93c785abc2f2369d1fd1a32910f39b95e8d50b3c
Jaiden Rothrock <jaiden_rothrock@hotmail.com>
2026-09-09 16:26:03 -0500

    Added Dog detail page

--- api/DTO/DogDto.cs
+++ api/DTO/DogDto.cs
@@ -2,7 +2,10 @@ namespace PartnerProjectDeShawn.api.DTO;
 
 public class DogDto
 {
-    public int Id {get;set;}
-    public string Name {get;set;}
-    public int CityId {get;set;}
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CityId { get; set; }
+    public int? WalkerId { get; set; }
+
+    public WalkerDto Walker { get; set; }
 }
--- client/src/apiManager.js
+++ client/src/apiManager.js
@@ -3,7 +3,19 @@ export const getGreeting = async () => {
   return res.json()
 }
 
+//Dogs
 export const getDogs = async () => {
   const res = await fetch("/api/dogs")
+  return res.json()
+}
+
+export const getDog = async (id) => {
+  const res = await fetch(`/api/dogs/${id}`)
+  return res.json()
+}
+
+//Walkers
+export const getWalkers = async () => {
+  const res = await fetch("/api/walkers")
   return res.json()
 }
